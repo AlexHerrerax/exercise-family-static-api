02111266c8fd6462b21c980f00b15652d6c92ea0
Alex Herrera <axherrerax@gmail.com>
2021-06-01 18:41:46 +0000

    Se crea rutas post put delete

--- src/app.py
+++ src/app.py
@@ -30,13 +30,48 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+#POST
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.json
+
+    if not member:
+        return jsonify({"Error":"No valido"}), 400
+    jackson_family.add_member(member)
+    return jsonify({"Success":"Agregado"}), 200
+
+
+#Id
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member),200
+    else:
+        return jsonify({"Error": "No existe"}), 400
+
+
+#PUT
+@app.route('/member/<int:member_id>', methods=['PUT'])
+def update_member(member_id):
+    member = request.json
+    if not member:
+        return jsonify({"Error":"No valido"}), 400
+    jackson_family.update_member(member_id, member)
+    return jsonify(member)
+
+#Delete
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    if not member:
+        return jsonify({"Error":"No existe"})
+    return jsonify(member)
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
